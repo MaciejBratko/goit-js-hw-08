fecccb524ee339bc68f81f5d288a82f19c130a53
hmmm <157412862+MaciejBratko@users.noreply.github.com>
2024-06-20 15:09:14 +0000

    ładowało kod zanim się załadował HTML, teraz już działa OK

--- gallery.js
+++ gallery.js
@@ -1,45 +1,47 @@
-const images = [
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
-    description: "Hokkaido Flower",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-    description: "Container Haulage Freight",
-  },
-  // Pozostałe obiekty obrazów...
-];
+document.addEventListener("DOMContentLoaded", () => {
+  const images = [
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+      description: "Hokkaido Flower",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+      description: "Container Haulage Freight",
+    },
+    // Pozostałe obiekty obrazów...
+  ];
 
-const gallery = document.querySelector("ul.gallery");
-gallery.innerHTML = images
-  .map(
-    ({ preview, original, description }) => `
+  const gallery = document.querySelector("ul.gallery");
+  gallery.innerHTML = images
+    .map(
+      ({ preview, original, description }) => `
 <li class="gallery-item">
 <a class="gallery-link" href="${original}">
 <img class="gallery-image" src="${preview}" data-source="${original}" alt="${description}" />
 </a>
 </li>`
-  )
-  .join("");
+    )
+    .join("");
 
-gallery.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.target.nodeName !== "IMG") return;
-  const lightboxInstance = basicLightbox.create(`
+  gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return;
+    const lightboxInstance = basicLightbox.create(`
 <img width="1400" height="900" src="${event.target.dataset.source}">
 `);
-  lightboxInstance.show();
-  const onKeyPress = (event) => {
-    if (event.key === "Escape") {
-      lightboxInstance.close();
-      document.removeEventListener("keydown", onKeyPress);
-    }
-  };
-  document.addEventListener("keydown", onKeyPress);
+    lightboxInstance.show();
+    const onKeyPress = (event) => {
+      if (event.key === "Escape") {
+        lightboxInstance.close();
+        document.removeEventListener("keydown", onKeyPress);
+      }
+    };
+    document.addEventListener("keydown", onKeyPress);
+  });
 });
